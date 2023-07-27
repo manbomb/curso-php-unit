ef1fa803177ca4991849803cc50dbf2331fb67a0
Sérgio Avilla <sergiolucasavilladasilva@gmail.com>
2023-07-27 07:27:14 -0300

    add tests

--- src/Service/Avaliador.php
+++ src/Service/Avaliador.php
@@ -2,11 +2,22 @@
 
 namespace Alura\Leilao\Service;
 
+use Alura\Leilao\Model\Lance;
 use Alura\Leilao\Model\Leilao;
+
 class Avaliador
 {
     private $maiorValor = -INF;
     private $menorValor = INF;
+    private $maioresLances;
+
+    /**
+     * @return Lance[]
+     */
+    public function getMaioresLances(): array
+    {
+        return $this->maioresLances;
+    }
 
     public function avalia(Leilao $leilao): void
     {
@@ -19,6 +30,11 @@ class Avaliador
                 $this->menorValor = $lance->getValor();
             }
         }
+        $lances = $leilao->getLances();
+        usort($lances, function (Lance $lance1, Lance $lance2) {
+            return $lance2->getValor() - $lance1->getValor();
+        });
+        $this->maioresLances = array_slice($lances, 0, 3);
     }
 
     public function getMaiorValor(): float
